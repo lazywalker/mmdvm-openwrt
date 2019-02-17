b4ec5ba8c6512e91c86279cd84fae3a790ae1ce0
lazywalker <lazywalkerz@gmail.com>
2019-02-17 21:58:20 +0800

    initial commit

---
+++ p25-clients/files/usr/sbin/P25HostsUpdate.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+###################################################
+#                                                 #
+# P25Hosts                                        #
+#                                                 #
+###################################################
+
+wget -O /etc/mmdvm/P25Hosts.txt -q "https://github.com/g4klx/P25Clients/raw/master/P25Gateway/P25Hosts.txt"
---
+++ ysf-clients/files/usr/sbin/FCSRoomsUpdate.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+###################################################
+#                                                 #
+# FCSRooms                                        #
+#                                                 #
+###################################################
+
+wget -O /etc/mmdvm/FCSRooms.txt -q "https://github.com/g4klx/YSFClients/raw/master/YSFGateway/FCSRooms.txt"
