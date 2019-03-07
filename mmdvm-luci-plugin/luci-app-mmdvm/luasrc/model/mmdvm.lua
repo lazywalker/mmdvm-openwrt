9274c65b2d84e6b5ca9004702ca2d86950d969b9
lazywalker <lazywalkerz@gmail.com>
2019-03-07 17:35:51 +0800

    implement lastheard detail panel

--- mmdvm-luci-plugin/luci-app-mmdvm/luasrc/model/mmdvm.lua
+++ mmdvm-luci-plugin/luci-app-mmdvm/luasrc/model/mmdvm.lua
@@ -44,163 +44,132 @@ local function get_hearlist(loglines)
 		local logline = loglines[i]
 		-- remoing invaild lines
 		repeat
-			if string.find(logline, "BS_Dwn_Act") then
+			if string.find(logline, "BS_Dwn_Act") or
+				string.find(logline, "invalid access") or
+				string.find(logline, "received RF header for wrong repeater") or
+				string.find(logline, "unable to decode the network CSBK") or
+				string.find(logline, "overflow in the DMR slot RF queue") or
+				string.find(logline, "non repeater RF header received") or
+				string.find(logline, "Embedded Talker Alias") or 
+				string.find(logline, "DMR Talker Alias") or
+				string.find(logline, "CSBK Preamble") or
+				string.find(logline, "Preamble CSBK")
+			then
 				break
-			end
-		until true
-		repeat
-			if string.find(logline, "invalid access") then
-				break
-			end
-		until true
-		repeat
-			if string.find(logline, "received RF header for wrong repeater") then
-				break
-			end
-		until true
-		repeat
-			if string.find(logline, "unable to decode the network CSBK") then
-				break
-			end
-		until true
-		repeat
-			if string.find(logline, "overflow in the DMR slot RF queue") then
-				break
-			end
-		until true
-		repeat
-			if string.find(logline, "non repeater RF header received") then
-				break
-			end
-		until true
-		repeat
-			if string.find(logline, "Embedded Talker Alias") then
-				break
-			end
-		until true
-		repeat
-			if string.find(logline, "DMR Talker Alias") then
-				break
-			end
-		until true
-		repeat
-			if string.find(logline, "CSBK Preamble") then
-				break
-			end
-		until true
-		repeat
-			if string.find(logline, "Preamble CSBK") then
-				break
-			end
-		until true
-
-		local mode = string.sub(logline, 28, string.find(logline, ",")-1)
-
-		if string.find(logline, "end of") 
-			or string.find(logline, "watchdog has expired")
-			or string.find(logline, "ended RF data")
-			or string.find(logline, "ended network")
-			or string.find(logline, "RF user has timed out")
-			or string.find(logline, "transmission lost")
-		then
-			local linetokens = util.split(logline, ", ")
-			local count_tokens = (linetokens and #linetokens) or 0
-
-			if string.find(logline, "RF user has timed out") then
-				duration = "TOut"
-				ber = "??"
-			else
-				if count_tokens >= 3 then
-					duration = linetokens[3]
-				end
-				if count_tokens >= 4 then
-					loss = linetokens[4]
-				end
 			end
 
-			-- if RF-Packet, no LOSS would be reported, so BER is in LOSS position
-			if string.find(loss, "BER") == 1 then
-				ber = string.trim(string.sub(loss, 6))
-				loss = "0"
-				-- TODO: RSSI
-			else
-				loss = string.trim(string.sub(loss, 1, -14))
-				if count_tokens >= 5 then
-					ber = string.trim(string.sub(linetokens[5], 6, -2))
-				end
-			end
+			local mode = string.sub(logline, 28, string.find(logline, ",")-1)
 
-			-- table.insert(headlist, loss)
-			-- table.insert(headlist, ber)
-			-- table.insert(headlist, duration)
+			if string.find(logline, "end of") 
+				or string.find(logline, "watchdog has expired")
+				or string.find(logline, "ended RF data")
+				or string.find(logline, "ended network")
+				or string.find(logline, "RF user has timed out")
+				or string.find(logline, "transmission lost")
+			then
+				local linetokens = util.split(logline, ", ")
+				local count_tokens = (linetokens and #linetokens) or 0
 
---[[]
-
-			if string.find(logline, "ended RF data") or string.find(logline, "ended network") then
-				if mode == "DMR Slot 1" then ts1duration = "SMS" 
-				elseif mode == "DMR Slot 2" then ts2duration = "SMS" 
-				end
-			else
-				local switch = {
-					["DMR Slot 1"] = function()
-						ts1duration = duration
-						ts1loss = loss
-						ts1ber = ber
-						ts1rssi = rssi
-					end,
-					["DMR Slot 2"] = function()
-						ts2duration = duration
-						ts2loss = loss
-						ts2ber = ber
-						ts2rssi = rssi
-					end,
-					["YSF"] = function()
-						ysfduration = duration
-						ysfloss = loss
-						ysfber = ber
-						ysfrssi = rssi
-					end,
-					["P25"] = function()
-						p25duration = duration
-						p25loss = loss
-						p25ber = ber
-						p25rssi = rssi
+				if string.find(logline, "RF user has timed out") then
+					duration = "TOut"
+					ber = "??"
+				else
+					if count_tokens >= 3 then
+						duration = string.trim(string.sub(linetokens[3], 1, string.find(linetokens[3], " ")))
 					end
-				}
-				local f = switch[mode]
-				if(f) then f() end
+					if count_tokens >= 4 then
+						loss = linetokens[4]
+					end
+				end
+
+				-- if RF-Packet, no LOSS would be reported, so BER is in LOSS position
+				if string.find(loss, "BER") == 1 then
+					ber = string.trim(string.sub(loss, 6))
+					loss = "0"
+					-- TODO: RSSI
+				else
+					loss = string.trim(string.sub(loss, 1, -14))
+					if count_tokens >= 5 then
+						ber = string.trim(string.sub(linetokens[5], 6, -2))
+					end
+				end
+
+				-- table.insert(headlist, loss)
+				-- table.insert(headlist, ber)
+				-- table.insert(headlist, duration)
+
+	--[[]
+
+				if string.find(logline, "ended RF data") or string.find(logline, "ended network") then
+					if mode == "DMR Slot 1" then ts1duration = "SMS" 
+					elseif mode == "DMR Slot 2" then ts2duration = "SMS" 
+					end
+				else
+					local switch = {
+						["DMR Slot 1"] = function()
+							ts1duration = duration
+							ts1loss = loss
+							ts1ber = ber
+							ts1rssi = rssi
+						end,
+						["DMR Slot 2"] = function()
+							ts2duration = duration
+							ts2loss = loss
+							ts2ber = ber
+							ts2rssi = rssi
+						end,
+						["YSF"] = function()
+							ysfduration = duration
+							ysfloss = loss
+							ysfber = ber
+							ysfrssi = rssi
+						end,
+						["P25"] = function()
+							p25duration = duration
+							p25loss = loss
+							p25ber = ber
+							p25rssi = rssi
+						end
+					}
+					local f = switch[mode]
+					if(f) then f() end
+				end
+	]]
+
 			end
- ]]
 
-		end
+			local timestamp = string.sub(logline, 4, 22)
+			local callsign, target
+			if string.find(logline, "from") then
+				callsign = string.trim(string.sub(logline, string.find(logline, "from")+5, string.find(logline, "to") - 2))
+				target = string.trim(string.sub(logline, string.find(logline, "to") + 3))
+				if mode ==  "YSF" then
+					target = string.trim(string.sub(target, 14))
+				end
+			end
+			local source = "RF"
+			if string.find(logline, "network") then
+				source = "Net"
+			end
 
-		local timestamp = string.sub(logline, 4, 22)
-		local callsign, target
-		if string.find(logline, "from") then
-			callsign = string.trim(string.sub(logline, string.find(logline, "from")+5, string.find(logline, "to") - 2))
-			target = string.trim(string.sub(logline, string.find(logline, "to") + 3))
-		end
-		local source = "RF"
-		if string.find(logline, "network") then
-			source = "Net"
-		end
+			-- Callsign or ID should be less than 11 chars long, otherwise it could be errorneous
+			if callsign and #callsign < 11 then
+				table.insert(headlist, 
+					{
+						timestamp = timestamp, 
+						mode = mode, 
+						callsign = callsign, 
+						target = target, 
+						source = source,
+						duration = duration,
+						loss = tonumber(loss) or 0,
+						ber = tonumber(ber) or 0,
+						rssi = rssi
+					}
+				)
+			end
 
-		-- Callsign or ID should be less than 11 chars long, otherwise it could be errorneous
-		if callsign and #callsign < 11 then
-			table.insert(headlist, 
-				{
-					timestamp = timestamp, 
-					mode = mode, 
-					callsign = callsign, 
-					target = target, 
-					source = source,
-					duration = duration,
-					loss = tonumber(loss) or 0,
-					ber = tonumber(ber) or 0,
-					rssi = rssi
-				}
-			)
-		end
+		until true -- end repeat
 	end -- end loop
 
 	-- table.insert(headlist, 
