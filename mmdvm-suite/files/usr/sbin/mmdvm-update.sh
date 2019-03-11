756ab608112f554c3f5adde5df263cdcbac07a93
lazywalker <lazywalkerz@gmail.com>
2019-03-11 16:03:17 +0800

    add mmdvm-suite package

---
+++ mmdvm-suite/files/usr/sbin/mmdvm-update.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# 
+# Copyright 2019 BD7MQB (bd7mqb@qq.com)
+# This is free software, licensed under the GNU GENERAL PUBLIC LICENSE, Version 2.0
+# 
+
+opkg update
+opkg upgrade mmdvm-suite mmdvm-luci-plugin mmdvm-host p25-clients ysf-clients
+
+# TODO: Hostfiles update goes here
