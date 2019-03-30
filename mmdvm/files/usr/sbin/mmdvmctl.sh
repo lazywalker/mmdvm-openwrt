3ac14cf6a241ecf9d74e197a491a41e66460d676
lazywalker <lazywalkerz@gmail.com>
2019-03-30 19:08:46 +0800

    add mmdvmctl.sh the control script for mmdvm suite

---
+++ mmdvm/files/usr/sbin/mmdvmctl.sh
@@ -0,0 +1,60 @@
+#!/bin/sh /etc/rc.common
+# 
+# Copyright 2019 BD7MQB <bd7mqb@qq.com>
+# This is free software, licensed under the GNU GENERAL PUBLIC LICENSE, Version 2.0
+# 
+# The control script of mmdvm services
+#
+
+SERVICE_WRITE_PID=0
+SERVICE_DAEMONIZE=0
+HELP="ss"
+EXTRA_COMMANDS="update"                                                                                        
+EXTRA_HELP="	update  Upgrade the mmdvm suite to lastest version" 
+
+help() {
+	cat <<EOF
+Syntax: $initscript [command]
+
+* This is the control script of mmdvm services (mmdvmhost, p25gateway, p25parrot, ysfgateway, ysfparrot).
+* For pkg update, run 'mmdvmctl update'
+
+Available commands:
+	start	Start the mmdvm services
+	stop	Stop the mmdvm services
+	restart	Restart the mmdvm services
+	enable	Enable mmdvm services autostart
+	disable	Disable mmdvm services autostart
+$EXTRA_HELP
+EOF
+}
+
+_command() {
+    /etc/init.d/p25gateway $1
+    /etc/init.d/p25parrot $1
+    /etc/init.d/ysfgateway $1
+    /etc/init.d/ysfparrot $1
+    /etc/init.d/mmdvmhost $1
+    /etc/init.d/dmrid $1
+}
+
+start() {
+    _command start
+}
+
+stop() {
+    _command stop
+}
+
+enable() {
+    _command enable
+}
+
+disable() {
+    _command disable
+}
+
+update() {
+    opkg update
+    opkg upgrade mmdvm mmdvm-luci mmdvm-luci-i18n-zh-cn mmdvm-host p25-clients ysf-clients
+}
