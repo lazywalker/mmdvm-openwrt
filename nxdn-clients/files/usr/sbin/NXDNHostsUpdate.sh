d6e90f474a2877fd0cb6102c902870c3ac522a06
lazywalker <lazywalkerz@gmail.com>
2019-04-07 01:23:55 +0800

    add package nxdn-clients

---
+++ nxdn-clients/files/usr/sbin/NXDNHostsUpdate.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+# 
+# Copyright 2019 BD7MQB <bd7mqb@qq.com>
+# This is free software, licensed under the GNU GENERAL PUBLIC LICENSE, Version 2.0
+# 
+
+wget -O /etc/mmdvm/NXDNHosts.txt -q "http://github.com/g4klx/NXDNClients/raw/master/NXDNGateway/NXDNHosts.txt"
+
+# TODO: a way to fetch formated NXDN.csv from radioid.net
