56a5e951812d35b1226f2ef92b28d136232fbca4
lazywalker <lazywalkerz@gmail.com>
2020-08-26 01:28:17 +0800

    update host files

--- nxdn-clients/files/usr/sbin/NXDNHostsUpdate.sh
+++ nxdn-clients/files/usr/sbin/NXDNHostsUpdate.sh
@@ -6,5 +6,6 @@
 # 
 
 wget -O /etc/mmdvm/NXDNHosts.txt -q "http://github.com/g4klx/NXDNClients/raw/master/NXDNGateway/NXDNHosts.txt"
+wget -O /etc/mmdvm/NXDN.csv -q "http://github.com/g4klx/NXDNClients/raw/master/NXDNGateway/NXDN.csv"
 
 # TODO: a way to fetch formated NXDN.csv from radioid.net
